87798df3413ded3bda56388ad8bce8b18de8bb78
John Rose <john.rose3@comcast.net>
2020-03-18 07:20:00 -0700

    Create page-folio.php
    Page with paths to various text and image presentation pages

---
+++ pages/page-folio.php
@@ -0,0 +1,9 @@
+<?php if (!defined('PERCH_RUNWAY')) include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); ?>
+	<?php perch_layout('global/header'); ?>
+		<?php perch_content('Page Heading'); ?>
+			<?php perch_content('Main Navigation'); ?>
+				<?php perch_content('Timely Alert'); ?>
+					<?php perch_content('Page Content'); ?>
+						<?php perch_content('Folio'); ?>
+							<?php perch_content('Link to Top of Page'); ?>
+								<?php perch_layout('global/footer'); ?>
