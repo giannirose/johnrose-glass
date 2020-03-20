d6509d3042deb86a209115289cb9a147e9fab420
John Rose <john.rose3@comcast.net>
2020-03-20 06:54:48 -0700

    Create component.php
    Used, for example, on the public projects page

---
+++ pages/component.php
@@ -0,0 +1,8 @@
+<?php if (!defined('PERCH_RUNWAY')) include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); ?>
+	<?php perch_layout('global/header'); ?>
+		<?php perch_content('Page Heading') ?>
+			<?php perch_content('Main Navigation'); ?>
+				<?php perch_content('Timely Alert'); ?>
+					<?php perch_content('Text Area'); ?>
+						<?php perch_content('Component'); ?>
+	<?php perch_layout('global/footer'); ?>
